e5d70ed41127c699142542fa748ea5ae2f95cb1e
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-01 23:04:02 +0300

    Fixed combining styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import { useState } from "react";
-
+import React, { useState } from "react";
 const styles = {
   bold: { fontWeight: "bold" },
   italic: { fontStyle: "italic" },
@@ -12,16 +11,47 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
-  let [text, setText] = useState("black");
-  const [style, setStyle] = useState("");
-  const [color, setColor] = useState("");
+  const [textArea, setTextArea] = useState({
+    color: "",
+    fontStyle: "",
+    fontWeight: "",
+    textDecorationLine: "",
+    fontSize: 40,
+  });
 
+  const setTextColor = (color) => {
+    setTextArea((previousTextArea) => ({
+      ...previousTextArea,
+      color: color,
+    }));
+  };
+
+  const setTextFont = (Font, event) => {
+    const font = Object.keys(Font)[0];
+    if (textArea[font] === "") {
+      setTextArea((previousTextArea) => ({
+        ...previousTextArea,
+        ...Font,
+      }));
+      event.target.className = "btn btn-primary";
+    } else {
+      const newStyle = {};
+      newStyle[font] = "";
+      setTextArea((previousTextArea) => ({
+        ...previousTextArea,
+        ...newStyle,
+      }));
+      event.target.className = "btn btn-light";
+    }
+  };
+
+  //set button style
   const stylingBoxes = stylings.map((style) => (
     <button
       className="btn btn-light"
       style={styles[style]}
       key={style}
-      onClick={() => setStyle(style)}
+      onClick={(event) => setTextFont(styles[style], event)}
     >
       {style}
     </button>
@@ -31,7 +61,7 @@ function App() {
     <button
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
-      onClick={() => setColor(color)}
+      onClick={() => setTextColor(color)}
     />
   ));
 
@@ -40,16 +70,10 @@ function App() {
       <h1 className="title">Text Editor</h1>
       <div className="my-3">{stylingBoxes}</div>
       <textarea
-        rows="4"
-        cols="50"
-        style={{
-          color: color,
-          fontWeight: style.includes("bold") ? "bold" : "",
-          fontStyle: style.includes("italic") ? "italic" : "",
-          textDecorationLine: style.includes("underline") ? "underline" : "",
-          fontSize: 30,
-        }}
-        onChange={(event) => setText(event.target.value)}
+        placeholder="What's on your mind?"
+        rows="5"
+        cols="60"
+        style={textArea}
       />
       <div className="my-3">{colorBoxes}</div>
     </div>
